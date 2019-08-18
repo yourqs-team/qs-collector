70f67b2a62044c6432bdafc38c77aa20714f19ad
Roald Ignacio <roaldjap@gmail.com>
2019-08-19 00:00:28 +1200

    Update Association of User to Role

--- models/user.js
+++ models/user.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   User.associate = function(models) {
     // associations can be defined here
+    User.hasOne(models.Role);
   };
   return User;
 };
