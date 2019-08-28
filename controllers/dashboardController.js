7f85e3ec1451491a4001571a38f2b6ccd374f4e4
Roald Ignacio <roaldjap@gmail.com>
2019-08-29 00:50:31 +1200

    Define new models and use async and await for promises

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -6,6 +6,26 @@ const User = models.User;
 const Role = models.Role;
 const Project = models.Project;
 
+// Second Level
+const Manpower = models.Manpower;
+const SafetyRequirement = models.Safety_requirement;
+const TemporaryService = models.Temporary_service;
+const SiteArrangement = models.Site_arrangement;
+const AllowanceAndInsurance = models.Allowance_and_insurance;
+const ProffesionalServiceAllowance = models.Proffesional_service_allowance;
+const Interior = models.Interior;
+const Exterior = models.Exterior;
+
+// Third Level
+const HardLandscaping = models.Hard_landscaping;
+const WindowAndDoor = models.Window_and_door;
+const JoineryAllowance = models.Joinery_allowance;
+const Electrical = models.Electrical;
+const InteriorFinish = models.InteriorFinish;
+const Plumbing = models.Plumbing;
+const Drainage = models.Drainage;
+const Other = models.Other;
+
 exports.projects = async (req, res) => {
   // Select all projects
   const projects = await Project.findAll({
@@ -23,7 +43,16 @@ exports.editProject = async (req, res) => {
   let project_id = hashids.decode(req.params); // returns primary key of project
 
   //2. Select the project
-  const project = await Project.findOne({where: project_id});
+  const project = await Project.findOne({where: project_id, include: [
+    {model: Manpower},
+    {model: SafetyRequirement},
+    {model: TemporaryService},
+    {model: SiteArrangement},
+    {model: AllowanceAndInsurance},
+    {model: ProffesionalServiceAllowance},
+    {model: Interior},
+    {model: Exterior}
+  ]});
 
   //3. Render
   res.json(project);
