f750f954da01ce89eb8f3959038ae7cc449d253b
Marlon Steve <marlonspr@hotmail.com>
2019-08-28 19:50:22 +1200

    registration

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,9 +1,45 @@
-exports.registerForm = (req, res) => {
-  const orientation = [
-    'Male', 
-    'Female', 
-    'Rather not to say'
-  ];
+const User = require("../models").User;
 
-  res.render('register', {title: 'Register', orientation});
-}
+exports.registerForm = (req, res) => {
+  const orientation = ["Male", "Female", "Rather not to say"];
+
+  res.render("register", { title: "Register", orientation });
+};
+
+exports.createUser = (req, res) => {
+  let {
+    username,
+    password,
+    confirmPass,
+    email,
+    contact,
+    firstname,
+    lastname,
+    gender,
+    birthday,
+    company,
+    address
+  } = req.body;
+
+  if (!username || !password || !email || !contact || !firstname) {
+    req.flash("error", "Please fill up all the fields");
+    res.render("register", { title: "Register", orientation });
+  }
+
+  // User.create({
+  //   username,
+  //   password,
+  //   email,
+  //   contact,
+  //   firstname,
+  //   lastname,
+  //   gender,
+  //   birthday,
+  //   company,
+  //   address
+  // }).then(() => {
+  //   User.findOrCreate({ where: { username } }).then(([user, created]) => {
+  //     console.log(user);
+  //   });
+  // });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
 // Define your CONTROLLERS here
-const sampleController = require('../controllers/sampleController');
-const loginController = require('../controllers/loginController');
-const registerController = require('../controllers/registerController');
+const sampleController = require("../controllers/sampleController");
+const loginController = require("../controllers/loginController");
+const registerController = require("../controllers/registerController");
 
 // see handlers/errorHandlers.js - this function catch all errors - EXPERIMENTAL
-const { catchErrors } = require('../handlers/errorHandlers');
+const { catchErrors } = require("../handlers/errorHandlers");
 
 /* Define your RESTful routes here */
-router.get('/', sampleController.homePage);
-router.get('/e', sampleController.emailSample);
-router.get('/users', sampleController.getUsers);
-router.get('/role', sampleController.getUserRole);
-router.get('/dashboard', loginController.isLoggedIn, sampleController.dashboard);
-
+router.get("/", sampleController.homePage);
+router.get("/e", sampleController.emailSample);
+router.get("/users", sampleController.getUsers);
+router.get("/role", sampleController.getUserRole);
+router.get(
+  "/dashboard",
+  loginController.isLoggedIn,
+  sampleController.dashboard
+);
 
 // login
-router.get('/login', loginController.loginForm);
-router.post('/login', loginController.login);
-router.get('/logout', loginController.logout);
+router.get("/login", loginController.loginForm);
+router.post("/login", loginController.login);
+router.get("/logout", loginController.logout);
 
 // Register
-router.get('/register', registerController.registerForm);
+router.get("/register", registerController.registerForm);
+router.post("/register", registerController.createUser);
 
 module.exports = router;
