e1dc50e2f15878b38baac4eb4edadc296478c2a7
Roald Ignacio <roaldjap@gmail.com>
2019-08-28 16:12:57 +1200

    Added associated fields

--- handlers/passport.js
+++ handlers/passport.js
@@ -1,13 +1,21 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+
+// Don't forget to include models here.
 const models = require('../models/index');
 const User = models.User;
+const Role = models.Role;
 
 // usernameField and passwordField must be defined in your models/users
 passport.use(new LocalStrategy( {usernameField: 'username', passwordField: 'password'},
   (username, password, done) => {
     // Using sequelize, select user from database
-    User.findOne({where:{username: username, password: password}})
+    User.findOne({
+      where:{username: username, password: password},
+      include:[
+        {model: Role}
+      ]
+    })
       .then( function(user){
         // see models/users.js for validPassword InstanceMethod
         if (!user) {
