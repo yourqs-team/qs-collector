5078a54282ac341294f024b398de5b0f3d2ae9f7
Roald Ignacio <roaldjap@gmail.com>
2019-09-05 14:17:05 +1200

    Deserialize user as user and not obj

--- handlers/passport.js
+++ handlers/passport.js
@@ -36,8 +36,8 @@ passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+passport.deserializeUser(function(user, cb) {
+  cb(null, user);
 });
 
 module.exports = passport;
