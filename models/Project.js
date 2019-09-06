6403551bb48cc536d4e72300dee350d5d60bddb4
Roald Ignacio <roaldjap@gmail.com>
2019-09-07 09:47:35 +1200

    hooks true

--- models/Project.js
+++ models/Project.js
@@ -31,27 +31,27 @@ module.exports = (sequelize, DataTypes) => {
     Project.belongsTo(models.User, {foreignKey: 'UserId'});
 
     //Project
-    Project.hasOne(models.Manpower, {onDelete: 'cascade'});
-    Project.hasOne(models.Site_arrangement, {onDelete: 'cascade'});
-    Project.hasOne(models.Safety_requirement, {onDelete: 'cascade'});
-    Project.hasOne(models.Allowance_and_insurance, {onDelete: 'cascade'});
-    Project.hasOne(models.Temporary_service, {onDelete: 'cascade'});
-    Project.hasOne(models.Proffesional_service_allowance, {onDelete: 'cascade'});
+    Project.hasOne(models.Manpower, {onDelete: 'cascade', hooks: true});
+    Project.hasOne(models.Site_arrangement, {onDelete: 'cascade', hooks: true});
+    Project.hasOne(models.Safety_requirement, {onDelete: 'cascade', hooks: true});
+    Project.hasOne(models.Allowance_and_insurance, {onDelete: 'cascade', hooks: true});
+    Project.hasOne(models.Temporary_service, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Proffesional_service_allowance, {onDelete: 'cascade' , hooks: true});
 
     //Exterior
-    Project.hasOne(models.Exterior, {onDelete: 'cascade'});
-    Project.hasOne(models.Hard_landscaping, {onDelete: 'cascade'});
+    Project.hasOne(models.Exterior, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Hard_landscaping, {onDelete: 'cascade' , hooks: true});
     
     //Interior
-    Project.hasOne(models.Interior, {onDelete: 'cascade'});
-    Project.hasOne(models.Interior_trim, {onDelete: 'cascade'});
-    Project.hasOne(models.Interior_finish, {onDelete: 'cascade'});
-    Project.hasOne(models.Window_and_door, {onDelete: 'cascade'});
-    Project.hasOne(models.Joinery_allowance, {onDelete: 'cascade'});
-    Project.hasOne(models.Electrical, {onDelete: 'cascade'}); // added this
-    Project.hasOne(models.Plumbing, {onDelete: 'cascade'});
-    Project.hasOne(models.Drainage, {onDelete: 'cascade'});
-    Project.hasOne(models.Other, {onDelete: 'cascade'});
+    Project.hasOne(models.Interior, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Interior_trim, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Interior_finish, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Window_and_door, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Joinery_allowance, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Electrical, {onDelete: 'cascade' , hooks: true}); // added this
+    Project.hasOne(models.Plumbing, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Drainage, {onDelete: 'cascade' , hooks: true});
+    Project.hasOne(models.Other, {onDelete: 'cascade' , hooks: true});
   };
   return Project;
 };
